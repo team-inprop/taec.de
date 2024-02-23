1878515f018a0fad6951628c3e7c32566a0be511
Tor Inge <43708528+tormok@users.noreply.github.com>
2024-02-23 16:36:18 +0100

    added sponsor text in footer above logos

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 const Header: React.FC = () => {
     return (
         <footer className="mt-auto footer p-6 md:p-12 footer-bg-color">
+            <p className="text-center mb-4">Unterstützt durch:</p>
             <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 place-items-center">
                 <div className="scholarship-partner p-1 mb-2">
                     <Image height={150} width={150} alt="Kofinanziert von der Europäischen Union" title="Kofinanziert von der Europäischen Union" src="/media/kofinanziert-von-der-europaischen-union.png" />
